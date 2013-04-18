751861323d532e030d829073d27cdf4ba90a8bff
Kaiesh Vohra <kaiesh@kaiesh.com>
2013-04-18 07:49:05 +0530

    WIP - Tested working, now to package better

--- site-online/web/content/upload.php
+++ site-online/web/content/upload.php
@@ -1,35 +1,40 @@
 <?
 //Configure Rackspace relevant variables
 define('RAXSDK_OBJSTORE_NAME','cloudFiles');
-define('RAXSDK_OBJSTORE_REGION','LON');
+define('RAXSDK_OBJSTORE_REGION','DFW');
 define('RAXSDK_TIMEOUT',600);
 //Load the Rackspace libraries
 require_once('../lib/php-opencloud/lib/rackspace.php');
-use OpenCloud;
+
 /**
  * Configure the server variables
  **/
-define("AUTHURL", "https://lon.identity.api.rackspacecloud.com/v2.0/");
+define("AUTHURL", RACKSPACE_US); //This will differ based on which country your account is in
+//Your Rackspace username
 define("USERNAME", "your-username");
-define("APIKEY", "your-apikey");
-define("CONTAINERNAME", "your-containername");
+//Your rackspace API key generated from http://mycloud.rackspace.com/a/<account-name>/api-keys
+define("APIKEY", "your-key");
+//Your container name as defined during creation
+define("CONTAINERNAME", "your-container-name");
 /** END OF SERVER VARS CONFIG **/
+
 
 //establish the connection
 $connection = new \OpenCloud\Rackspace(AUTHURL,
-									   array('username' = >USERNAME,
-											 'apiKey' => APIKEY));
-//create the object store
-$ostore = $connection->ObjectStore();
+							array('username' =>USERNAME,
+								  'apiKey' => APIKEY));
+
+//create the object store, explictly specify location
+$ostore = $connection->ObjectStore('cloudFiles', 'DFW'); //This will differ depending on where your container is geographically located
 
 //test a file upload
 $testData = array("container_name"=>CONTAINERNAME,
 				  "filename"=>sha1("./sample.pdf"),
 				  "filepath"=>"./sample.pdf");
 
-$result = upload_file_get_url($testData);
+$result = upload_file_get_url($testData, $ostore);
 if ($result["status"]){
-	echo "Success! Your file can be found at ".$result["url"];
+	echo "Success! Your file can be found at ".$result["url"]." -OR- on the CDN at: ".$result["cdn"];
 }else{
 	echo "FAIL! Error returned: ".$result["err"];
 }
@@ -44,10 +49,11 @@ if ($result["status"]){
  * 
  * Returns array with following structure:
  * array("status" => true | false, //true if upload success, false otherwise
- *       "url" => "<URL>", //only returned if successful
+ *       "url" => "<URL>", //static URL only returned if successful
+ *       "cdn" => "<URL>", //CDN URL returned if successful
  *       "err" => "<Exception details>") //only returned if failed
  **/
-function upload_file_get_url($detailsArr){
+function upload_file_get_url($detailsArr, $ostore){
 	try{
 		$container = $ostore->Container($detailsArr["container_name"]);
 		$obj = $container->DataObject();
@@ -56,7 +62,8 @@ function upload_file_get_url($detailsArr){
 					 $detailsArr["filepath"]);
 		
 		return array("status"=>true,
-					 "url"=>$obj->PublicURL());
+					 "url"=>$obj->PublicURL(),
+					 "cdn"=>$obj->CDNUrl());
 	}catch (Exception $e){
 		return array("status"=>false,
 					 "err"=>$e->getMessage());
